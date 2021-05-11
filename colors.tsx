3f3568fe774e6e4bce0a260a148dca05dfafcb9d
Matthew Boeh <m@mboeh.com>
2021-05-10 22:35:41 -0700

    Doing some map stuff

---
+++ colors.tsx
@@ -0,0 +1,5 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="1.6.0" name="colors" tilewidth="24" tileheight="24" tilecount="4356" columns="8">
+ <image source="assets/colors.jpg" width="192" height="192"/>
+ <tile id="54" type="forest"/>
+</tileset>
---
+++ oryx_fan.tsx
@@ -0,0 +1,5 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="1.6.0" name="oryx_fan" tilewidth="24" tileheight="24" tilecount="2296" columns="56">
+ <image source="assets/16bitfan/oryx_16bit_fantasy_world_trans.png" width="1366" height="1007"/>
+ <tile id="57" type="wall"/>
+</tileset>
